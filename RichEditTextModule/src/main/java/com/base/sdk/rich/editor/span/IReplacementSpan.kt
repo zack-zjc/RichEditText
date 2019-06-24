a03a770fec4a25bd53679accafd7e2ba3a6ba3ec
zacky <1215442558@qq.com>
2019-06-24 10:46:24 +0800

    remove code

--- RichEditTextModule/src/main/java/com/base/sdk/rich/editor/span/IReplacementSpan.kt
+++
@@ -1,8 +0,0 @@
-package com.base.sdk.rich.editor.span
-
-/**
- * author:zack
- * Date:2019/5/31
- * Description:替换类的span
- */
-interface IReplacementSpan<E> :ISpan<E>
--- RichEditTextModule/src/main/java/com/base/sdk/rich/editor/span/IUnionTextSpan.kt
+++
@@ -1,8 +0,0 @@
-package com.base.sdk.rich.editor.span
-
-/**
- * author:zack
- * Date:2019/5/31
- * Description:一段文字是一个整体的span
- */
-interface IUnionTextSpan<E> :ISpan<E>
